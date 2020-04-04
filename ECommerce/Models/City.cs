412a1a65543a3343989c6a3e65cd6e7991924398
Jônatas Vitalino <jcv@cin.ufpe.br>
2020-04-03 23:38:11 -0300

    Add class City

---
+++ ECommerce/Models/City.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ECommerce.Models
+{
+    public class City
+    {   
+        [Key]
+        public int CityId { get; set; }
+        
+        [Required(ErrorMessage = "O campo Nome é obrigatório!")]
+        public String Name { get; set; }
+
+        public int DepartmentsId { get; set; }
+    }
+}
